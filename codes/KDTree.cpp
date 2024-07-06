236f789a713ee5a88078858ab2d625d74ac9c32e
Devaj Sanghvi <devajsanghvi15@gmail.com>
2024-07-06 22:08:41 +0530

    Update KDTree.cpp

--- codes/KDTree.cpp
+++ codes/KDTree.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int k = 3;
+const int k = 2;
 
 struct Node
 {
@@ -14,16 +14,15 @@ struct Node* newNode(int arr[])
 	struct Node* temp = new Node;
 
 	for (int i=0; i<k; i++)
-		temp->point[i] = arr[i];
+	temp->point[i] = arr[i];
 
 	temp->left = temp->right = NULL;
 	return temp;
 }
-
 Node *insertRec(Node *root, int point[], unsigned depth)
 {
 	if (root == NULL)
-		return newNode(point);
+	return newNode(point);
 
 	unsigned cd = depth % k;
 
@@ -34,7 +33,6 @@ Node *insertRec(Node *root, int point[], unsigned depth)
 
 	return root;
 }
-
 Node* insert(Node *root, int point[])
 {
 	return insertRec(root, point, 0);
@@ -48,7 +46,6 @@ bool arePointsSame(int point1[], int point2[])
 
 	return true;
 }
-
 bool searchRec(Node* root, int point[], unsigned depth)
 {
 	if (root == NULL)
@@ -66,24 +63,25 @@ bool searchRec(Node* root, int point[], unsigned depth)
 
 bool search(Node* root, int point[])
 {
+	// Pass current depth as 0
 	return searchRec(root, point, 0);
 }
 
 int main()
 {
 	struct Node *root = NULL;
-	int points[][k] = {{3, 6, 9}, {17, 15, 10}, {13, 15, 18}, {6, 12, 7},
-					{9, 1, 11}, {2, 7, 1}, {10, 19, 13}};
+	int points[][k] = {{3, 6}, {17, 15}, {13, 15}, {6, 12},
+					{9, 1}, {2, 7}, {10, 19}};
 
 	int n = sizeof(points)/sizeof(points[0]);
 
 	for (int i=0; i<n; i++)
-		root = insert(root, points[i]);
+	root = insert(root, points[i]);
 
-	int point1[] = {10, 19, 13};
+	int point1[] = {10, 19};
 	(search(root, point1))? cout << "Found\n": cout << "Not Found\n";
 
-	int point2[] = {12, 19, 13};
+	int point2[] = {12, 19};
 	(search(root, point2))? cout << "Found\n": cout << "Not Found\n";
 
 	return 0;
